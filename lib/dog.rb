8aa21b29ab360431317490d406395c4f2130fb4e
turpp <white.tripp@icloud.com>
2020-10-13 23:04:12 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require 'pry'
+class Dog
+attr_reader :name
+
+@@all = []
+
+def initialize(name)
+    @name=name
+    
+    self.save
+
+end
+
+def self.all
+    @@all
+end
+
+def self.print_all
+   @@all.each do |dogs|
+   puts "#{dogs.name}"
+   end
+end
+
+def self.clear_all
+    @@all.clear
+end
+
+def save
+    @@all << self
+end
+
+
+end
+
